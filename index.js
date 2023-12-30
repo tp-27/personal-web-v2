772156f84e270edb25045f66664ddcb1e8136d13
Thomas Phan <thomasphan@Thomass-Air.lan>
2023-12-30 14:06:00 -0500

    div info box bug fix

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-
-function showMenu() {
-    var menu = document.querySelector(".sidebar");
-    menu.style.display = "block";
-}
-
-function closeMenu() {
-    var menu = document.querySelector(".sidebar");
-    menu.style.display = "none";
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-
     // scroll animations
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) { // is entry intersecting viewport?
                 entry.target.classList.add('show'); // make entry visible
-                // entry.style.transition = "none";
-                // entry.style.WebkitTransition = "none";
             } 
         });
     });
@@ -33,34 +19,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const navObserver = new IntersectionObserver((entries) => {
         nav.classList.toggle("sticking", !entries[0].isIntersecting);
-        // console.log(entries);
     }); 
-
 
     navObserver.observe(scrollWatcher);
 
     const dropdownInfoBtn = document.querySelectorAll(".highlight-subheading");
-        
     dropdownInfoBtn.forEach(function(dropdown) {
-        dropdown.addEventListener('click', function(e) {
-            console.log(e);
-            if (dropdown.style.height != "80px") {
-                dropdown.style.height = "80px";
-                dropdown.style.alignItems = "center";
-                const content = dropdown.querySelector(".highlight-content");
-                content.style.display = "none";
-            } else {
-                dropdown.style.height = "auto";
-                dropdown.style.alignItems = "flex-start";
-
-                // dropdown.style.paddingTop = "25px";
-
-                const content = dropdown.querySelector(".highlight-content");
-                content.style.display = "block";
-                content.style.paddingTop = "50px";
-                content.style.paddingBottom = "25px";
-
-            }
+        dropdown.addEventListener('click', function() {
+            const dropdownContent = dropdown.querySelector('.highlight-content');
+            dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
         });
     });
 });
+
+function showMenu() {
+    var menu = document.querySelector(".sidebar");
+    menu.style.display = "block";
+}
+
+function closeMenu() {
+    var menu = document.querySelector(".sidebar");
+    menu.style.display = "none";
+}
